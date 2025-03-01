2876cb2bf2505c2621cd3a7b078a2e737a63be0b
Mattias Jansson <fimblo@yanson.org>
2025-03-01 23:44:33 +0100

    feat: dump tomorrows prices

---
+++ scripts/sql-tomorrows-spot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+start=$(date -d tomorrow "+%Y-%m-%d")
+end="${start}T23:59:59"
+
+cat<<EOF | sqlite3 -header -column database/spot_prices.db 
+SELECT STRFTIME('%m/%d %H:%M', time_start) AS hour, kWh_SEK
+    FROM spot_price
+    WHERE
+        DATETIME(time_start, '+0 hour') >= '$start'
+        AND
+        DATETIME(time_start, '+0 hour') < '$end'
+    ORDER BY time_start ASC
+EOF
+
